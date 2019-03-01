a5648cfc95c721a6f8799d4adf2e3113849d7f46
george33zmax <george33zmax@gmail.com>
2019-03-01 16:34:00 -0500

    adding wall app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./App.css";
+import { connect } from "react-redux";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      Aki en App Home
-      </div>
-    );
-  }
-}
 
-export default App;
+// pull here message
+import Messages from "./Wall";
+
+
+// prettier-ignore
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts, primaryColor, secondaryColor }) => (
+  <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
+    {/* wall */}
+    <Grid.Column >
+      <Messages />
+    </Grid.Column>
+
+    {/* Logo */}
+    <Grid.Column width={4}>
+      <div>aki logo</div>
+    </Grid.Column>
+  </Grid>
+);
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(App);
